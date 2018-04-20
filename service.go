ed176b25a0c6b87053d4fc40a5c28429f415f741
isigoing <tripolt@hm.edu>
2018-04-20 23:56:32 +0200

    updated service

--- service.go
+++ service.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"log"
 )
 
 func main() {
 	http.HandleFunc("/", helloWorld)
-	fmt.Printf("listening...")
-	log.Fatal(http.ListenAndServe(":80", nil))
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!!")
+	fmt.Fprintln(w, "Hello, World!!")
 }
