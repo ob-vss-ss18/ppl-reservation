1ba47a0fcab03cc5b549b9b12b8850de4e742c46
Nexus457 <maier14@hm.edu>
2018-06-05 11:29:16 +0200

    bug fixing

--- main.go
+++ main.go
@@ -18,27 +18,27 @@ func main() {
 		GraphiQL: true,
 	})
 
-	/*reservations := handler.New(&handler.Config{
+	reservations := handler.New(&handler.Config{
 		Schema:   &ReservationsSchema,
 		Pretty:   true,
 		GraphiQL: true,
 	})
 
-	reservation := handler.New(&handler.Config{
+	/*reservation := handler.New(&handler.Config{
 		Schema:   &ReservationSchema,
 		Pretty:   true,
 		GraphiQL: true,
 	})*/
 
 	http.Handle("/reserve", reserve)
-	//http.Handle("/reservations", reservations)
+	http.Handle("/reservations", reservations)
 	//http.Handle("/reservation", reservation)
 
 	http.HandleFunc("/hello", helloWorld)
 	fmt.Printf("listening...")
 
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
-	//log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), reservations))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), reservations))
 	//log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), reservation))
 
 
