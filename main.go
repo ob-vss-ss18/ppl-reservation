6ca078a01dabbf3e2ae2ffc0a215f7f2f2dc0fbc
Nexus457 <maier14@hm.edu>
2018-06-05 11:24:55 +0200

    bug fixing

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	initGraphQl()
 
 	reserve := handler.New(&handler.Config{
-		Schema:   &ReservationsSchema,
+		Schema:   &ReservationSchema,
 		Pretty:   true,
 		GraphiQL: true,
 	})
